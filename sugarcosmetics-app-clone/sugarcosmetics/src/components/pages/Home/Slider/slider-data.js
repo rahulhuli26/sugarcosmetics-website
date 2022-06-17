a7cfa9e30c6d78603de119a89f17fb5360bec14b
rahulhuli26 <rahulhulikoppe38@gmail.com>
2022-06-17 19:28:16 +0530

    Update slider-data.js

--- sugarcosmetics-app-clone/sugarcosmetics/src/components/pages/Home/Slider/slider-data.js
+++ sugarcosmetics-app-clone/sugarcosmetics/src/components/pages/Home/Slider/slider-data.js
@@ -1,12 +1,12 @@
 
 
 export const sliderData  = [
-    {image:"https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/d9103d2b-af25-40c3-b41b-90538502b315.gif",
+    {image:"https://d32baadbbpueqt.cloudfront.net/549c6869-246f-4dba-8cde-7a6798f866ec.jpg",
     heading: "",
     desc: "",
 },
 
-{image: "https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/22f5baf6-76b8-4315-84d1-29904960ea31.jpg",
+{image: "https://d32baadbbpueqt.cloudfront.net/Homepage/85bc9b9b-d8b3-4fa1-a1b0-a77ca9428664.jpg",
 heading: "",
 desc: ""
 
@@ -14,16 +14,16 @@ desc: ""
 
 
 
-{image: "https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/177972e0-4583-428b-b4db-da5546b71310.jpg",
+{image: "https://d32baadbbpueqt.cloudfront.net/fc27be93-0882-45d7-aef9-0ad461636eae.jpg",
 heading: "",
 desc: "",
 },
 
-{image:"https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/2726f64a-3b5b-4ca9-b066-2ed3d9245582.gif",
+{image:"https://d32baadbbpueqt.cloudfront.net/Homepage/70252059-0efb-40b0-b659-6d27afafa0ee.jpg",
 heading: "",
 desc: ""},
 
-{image:"https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/36e134cb-da00-481a-9769-b975af151b46.jpg",
+{image:"https://d32baadbbpueqt.cloudfront.net/Homepage/309d5880-c650-4773-9cb9-5b8efc593ac0.jpg",
 heading: "",
 desc: ""}
 ]
@@ -32,18 +32,18 @@ desc: ""}
 
 
 export const testData  = [
-    {image:"https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/b6c868ac-3497-4c45-906a-55bbb1ebbc0f.jpg",
+    {image:"https://d32baadbbpueqt.cloudfront.net/Homepage/11b811a4-2faa-4985-98f0-01af9f2df943.jpg",
     heading: "",
     desc: "",
 },
 
-{image: "https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/d8b1d3f0-7cbd-4697-ab5e-4dfdc5a5d408.jpg",
+{image: "https://d32baadbbpueqt.cloudfront.net/Homepage/fb9d5eba-b79a-4e38-b01d-fb6cab221874.jpg",
 heading: "",
 desc: ""
 
 },
 
-{image: "https://sugar-mobile-application.s3-ap-south-1.amazonaws.com/4453d766-3bd0-4fed-9a76-aa19dfc5f526.jpg",
+{image: "https://d32baadbbpueqt.cloudfront.net/Homepage/2cf0052d-e531-4f6e-9b00-e31f3642f0b3.jpg",
 heading: "",
 desc: "",
 }
